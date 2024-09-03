40ceae33c20f18f9471ce17d2cf2e364f627c81b
Marcus Loureiro <vh.machado.silva@gmail.com>
2024-09-03 01:15:43 -0300

    feat: layout inicial

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,5 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: ['@nuxt/eslint', '@nuxt/ui', ['@nuxtjs/google-fonts', {
+    families: {
+      Montserrat: [300, 400, 500, 600, 700, 800, 900],
+      K2D: [300, 400, 500, 600, 700, 800]
+    },
+    display: 'swap'
+  }]],
 })
