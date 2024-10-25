622630033a4546d4ef207c15489c9f5cfbd8f510
Marcus Loureiro <marcushuriel08@hotmail.com>
2024-10-25 18:50:27 -0300

    feat: EmotionAnalysis

--- backend/microservices/emotionAnalysis-service/app.js
+++ backend/microservices/emotionAnalysis-service/app.js
@@ -5,22 +5,21 @@ const os = require('os');
 
 
 
-// Middleware para analisar JSON
+
 app.use(express.json({ extended: false }));
 
-// Definir rotas
+
 app.use('/api', require('./routes/emotionAnalysis'));
 
 
-// Porta do serviço
 const PORT = process.env.PORT || 5003;
 
-// Obter dinamicamente o hostname (localhost ou domínio)
+
 const getHostName = () => {
     if (process.env.NODE_ENV === 'PRODUCAO') {
-        return os.hostname(); // Retorna o hostname do servidor em produção
+        return os.hostname(); 
     }
-    return 'localhost'; // Usar 'localhost' durante o desenvolvimento
+    return 'localhost'; 
 };
 
 // Iniciar o servidor
--- backend/microservices/emotionAnalysis-service/controllers/emotionAnalysisController.js
+++ backend/microservices/emotionAnalysis-service/controllers/emotionAnalysisController.js
@@ -1,16 +1,14 @@
 const { runGemini } = require("./gemini.js");
 
-// Função para gerar o prompt com base no texto de entrada
+
 function generatePrompt(review) {
   return `Retorne somente e nada mais além que um objeto json com um array com apenas as emoções identificadas nesse texto: '${review}'. Resuma cada emoção com uma palavra, não faça nenhuma explicação. Use somente essa lista de emoções: Joy, Sadness, Anger, Fear, Surprise, Disgust, Satisfaction, Anxiety, Disappointment, Doubt, Frustration, Shame, Despair, Enthusiasm, Relief, Discouragement, Guilt, Indifference, Envy, Grief. O objeto deve conter os atributos emotions, que será o array com as emoções em string, e approval, que é um score numérico para o texto utilizando a classificação Naive Bayes.`;
 }
 
-// Função para formatar o resultado de texto para JSON
+
 function formatResult(result) {
   try {
-    // Acesse o texto da parte
     const jsonString = result.candidates[0].content.parts[0].text.replace(/```json\n|\n```/g, '');
-    // Aqui espera-se que jsonString seja uma string JSON válida
     const formattedResult = JSON.parse(jsonString);
     return formattedResult;
   } catch (error) {
@@ -19,13 +17,11 @@ function formatResult(result) {
   }
 }
 
-// Função para calcular o escore médio de aprovação
 function computeAverageResults(analysisResults) {
   const totalApproval = analysisResults.reduce((acc, curr) => acc + curr.approval, 0);
   return totalApproval / analysisResults.length;
 }
 
-// Função para compilar as emoções por revisão
 function computeEmotionsByMovie(analysisResults) {
   const emotionsCount = {};
   
@@ -38,19 +34,19 @@ function computeEmotionsByMovie(analysisResults) {
   return emotionsCount;
 }
 
-// Função principal que executa a análise de emoções em várias revisões
+
 async function runEmotionAnalysis(reviews) {
   const analysisResults = [];
 
   for (const review of reviews) {
     const prompt = generatePrompt(review);
     
-    // Chamada ao runGemini
+
     const result = await runGemini(prompt);
     console.log("Resultado do runGemini:", result);
 
-    // Acesse o conteúdo das emoções
-    const formattedResult = formatResult(result); // Formate o conteúdo
+
+    const formattedResult = formatResult(result); 
     analysisResults.push(formattedResult);
   }
 
@@ -60,7 +56,7 @@ async function runEmotionAnalysis(reviews) {
   };
 }
 
-// Controlador que recebe a lista de revisões e executa a análise completa
+
 async function emotionAnalysis(req, res) {
   const { reviews } = req.body;
 
