b91159fd6685f41ac85da8e5ae93c3ad2de41b7b
Marcus Loureiro <marcushuriel08@hotmail.com>
2024-10-26 10:29:22 -0300

    feat: fila de requisições e rota de delete e rota de get pelo idReview

--- backend/microservices/emotion-analysis-service/routes/emotionAnalysis.js
+++ backend/microservices/emotion-analysis-service/routes/emotionAnalysis.js
@@ -1,8 +1,10 @@
 const express = require('express');
-const { emotionAnalysis } = require('../controllers/emotionAnalysisController');
-
+const { emotionAnalysis, getEmotionAnalysisByIdReview, deleteEmotionAnalysisById } = require("../controllers/emotionAnalysisController");
 const router = express.Router();
 
 router.post('/emotion-analysis', emotionAnalysis);
+router.get('/emotion-analysis/review/:idReview', getEmotionAnalysisByIdReview);
+router.delete('/emotion-analysis/:id', deleteEmotionAnalysisById);
+
 
 module.exports = router;
