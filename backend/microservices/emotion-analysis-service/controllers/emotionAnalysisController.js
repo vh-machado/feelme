9be4c5d81534d10b91c730b5491f96e6b3251d8e
Marcus Loureiro <marcushuriel08@hotmail.com>
2024-10-26 10:38:45 -0300

    refactor: impedir multiplas analises para um review

--- backend/microservices/emotion-analysis-service/controllers/emotionAnalysisController.js
+++ backend/microservices/emotion-analysis-service/controllers/emotionAnalysisController.js
@@ -5,18 +5,15 @@ let requestQueue = [];
 let processing = false;
 
 function enqueueRequest(request) {
-  
   requestQueue.push(request);
   processQueue();
 }
-
 
 async function processQueue() {
   if (processing || requestQueue.length === 0) return;
 
   processing = true;
   const { reviewText, idReview, res } = requestQueue.shift();
-
 
   try {
     const result = await runEmotionAnalysis(reviewText, idReview);
@@ -30,17 +27,14 @@ async function processQueue() {
   }
 }
 
-
 function generatePrompt(review) {
   return `Retorne somente e nada mais além que um objeto json com um array com apenas as emoções identificadas nesse texto: '${review}'. Cada emoção deve conter um campo "emoji" com um emoji representativo e um campo "description" com a descrição. Use somente esta lista de emoções: Alegria, Tristeza, Raiva, Medo, Surpresa, Nojo, Satisfação, Ansiedade, Decepção, Dúvida, Frustração, Vergonha, Desespero, Entusiasmo, Alívio, Desânimo, Culpa, Indiferença, Inveja e Luto.`;
 }
-
 
 function formatEmotions(result) {
   try {
     const jsonString = result.candidates[0].content.parts[0].text.replace(/```json\n|\n```/g, '');
     const parsedResult = JSON.parse(jsonString);
-
     return parsedResult; 
   } catch (error) {
     console.error("Erro ao formatar o resultado:", error);
@@ -48,29 +42,33 @@ function formatEmotions(result) {
   }
 }
 
-
 async function runEmotionAnalysis(reviewText, idReview) {
   const prompt = generatePrompt(reviewText);
   const result = await runGemini(prompt);
 
-
   const emotions = formatEmotions(result);
-  console.log(emotions)
+  console.log(emotions);
 
-  const savedAnalysis = await EmotionAnalysis.create({
-    idReview,
-    emotions,
-  });
 
-  return savedAnalysis;
+  const existingAnalysis = await EmotionAnalysis.findOne({ idReview });
+
+  if (existingAnalysis) {
+    existingAnalysis.emotions = emotions;
+    await existingAnalysis.save();
+    return existingAnalysis;
+  } else {
+    const savedAnalysis = await EmotionAnalysis.create({
+      idReview,
+      emotions,
+    });
+    return savedAnalysis;
+  }
 }
-
 
 async function emotionAnalysis(req, res) {
   const { reviewText, idReview } = req.body;
   enqueueRequest({ reviewText, idReview, res });
 }
-
 
 async function getEmotionAnalysisByIdReview(req, res) {
   const { idReview } = req.params;
@@ -85,7 +83,6 @@ async function getEmotionAnalysisByIdReview(req, res) {
     res.status(500).json({ error: "Erro ao buscar análise." });
   }
 }
-
 
 async function deleteEmotionAnalysisById(req, res) {
   const { id } = req.params;
