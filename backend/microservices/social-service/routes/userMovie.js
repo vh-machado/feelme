1abf268e8bcede6684040f3d1b53c7d962dc1eff
Marcus Loureiro <70539396+MarskyH@users.noreply.github.com>
2024-10-29 10:24:50 -0300

    Update userMovie.js

--- backend/microservices/social-service/routes/userMovie.js
+++ backend/microservices/social-service/routes/userMovie.js
@@ -1,5 +1,13 @@
 const express = require('express');
-const { getUserMovies, getAllUserReviews, getUserReviewsByMovie,  saveUserMovie, updateUserMovie, deleteUserMovie} = require('../controllers/userMovieController');
+const {
+  getUserMovies,
+  getAllUserReviews,
+  getUserReviewsByMovie,
+  saveUserMovie,
+  updateUserMovie,
+  deleteUserMovie,
+  checkUserMovie 
+} = require('../controllers/userMovieController');
 const router = express.Router();
 
 router.get('/userMovie/reviews/:userId', getAllUserReviews);
@@ -8,5 +16,6 @@ router.get('/userMovies', getUserMovies);
 router.post('/userMovie', saveUserMovie);
 router.put('/userMovie/:id', updateUserMovie);
 router.delete('/userMovie/:id', deleteUserMovie);
+router.get('/userMovie/check/:userId/:movieId', checkUserMovie);
 
 module.exports = router;
