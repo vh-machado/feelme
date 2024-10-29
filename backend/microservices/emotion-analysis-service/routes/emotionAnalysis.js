91bd340f18857be3327ec07296fe2967b258fe0f
Marcus Loureiro <70539396+MarskyH@users.noreply.github.com>
2024-10-29 10:50:18 -0300

    Update emotionAnalysis.js

--- backend/microservices/emotion-analysis-service/routes/emotionAnalysis.js
+++ backend/microservices/emotion-analysis-service/routes/emotionAnalysis.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const { emotionAnalysis, getEmotionAnalysisByReviewId, deleteEmotionAnalysisById } = require("../controllers/emotionAnalysisController");
+const { emotionAnalysis, getEmotionAnalysisByReviewId, deleteEmotionAnalysisById, getAllEmotionAnalysisByUserId } = require("../controllers/emotionAnalysisController");
 const router = express.Router();
 
 router.post('/emotion-analysis', emotionAnalysis);
 router.get('/emotion-analysis/review/:reviewId', getEmotionAnalysisByReviewId);
+router.get('/emotion-analysis/user/:userId', getAllEmotionAnalysisByUserId); // Nova rota para buscar análises por userId
 router.delete('/emotion-analysis/:id', deleteEmotionAnalysisById);
-
 
 module.exports = router;
